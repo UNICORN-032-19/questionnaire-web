1486093d5e8ee9db48881331d8fcb9c8ec3f3456
lkush <lkushniarevich@gmail.com>
2020-01-25 12:51:30 +0300

    Issue 4 Add Model Count

--- questionnaire/config/common.py
+++ questionnaire/config/common.py
@@ -15,6 +15,7 @@ class Common(Configuration):
         'django.contrib.sessions',
         'django.contrib.messages',
         'django.contrib.staticfiles',
+        'questionnaire.counts',
 
 
         # Third party apps
